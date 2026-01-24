e8c996c0aafc609d39888ceabb38ede6fcacb6ae
Kumyshbaeva Sofiya <kimsofocka10@gmail.com>
2026-01-24 15:03:10 +0300

    Добавьте файлы проекта.

---
+++ lib2122/DbConnection/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib2122.DbConnection
+{
+    public class ConnectionString
+    {
+        public static libraryKIUEntities libraryKIUEntities = new libraryKIUEntities();
+    }
+}
---
+++ lib2122/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using lib2122.DbConnection;
+
+namespace lib2122
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<Employee> employees {  get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //создаем контейнеры для того, что введет пользователь
+            string login = loginTb.Text.Trim();
+            string password = passwordPb.Password.Trim();
+
+            //заполняем динамический массив записями из бд
+            employees = new List<Employee>(ConnectionString.libraryKIUEntities.Employee.ToList());
+
+            //ищем нужного юзера
+            Employee currentUser = employees.FirstOrDefault(i => i.Login.Trim() == login && i.Password.Trim() == password);
+            if (currentUser != null)
+                MessageBox.Show("Good");
+            else
+                MessageBox.Show("Not good");
+
+
+        }
+    }
+}
